64b627df67b3b269180ede26f961180abc986400
Jaron Zijlstra <jaron_zijlstra@outlook.com>
2024-10-16 10:49:39 +0200

    feat: toevoegen deel generator op basis van demo

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,10 +1,34 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
+import nl.han.ica.icss.ast.*;
 
 public class Generator {
 	public String generate(AST ast) {
-        return "";
+		StringBuilder builder = new StringBuilder();
+        generateStylesheet(ast.root, builder);
+		return builder.toString();
+	}
+
+	private void generateStylesheet(Stylesheet stylesheet, StringBuilder sb) {
+		for (ASTNode child : stylesheet.getChildren()) {
+			if (child instanceof Stylerule) {
+				generateStylerule((Stylerule) child, sb);
+			}
+		}
+	}
+
+	private void generateStylerule(Stylerule stylerule, StringBuilder sb) {
+		sb.append(stylerule.selectors.get(0));
+		sb.append(" {\n");
+		for (ASTNode child : stylerule.body) {
+			sb.append("\t");
+			generateDeclaration((Declaration) child, sb);
+		}
+		sb.append("\n}");
+	}
+
+	private void generateDeclaration(Declaration declaration, StringBuilder sb) {
+		sb.append("Declaration");
 	}
 }
